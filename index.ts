c8174ba9a2292e824c6a3e4d19c428cb378b8772
Emon <emon.mhk69@gmail.com>
2025-05-07 21:48:12 +0600

    problem4 done

--- index.ts
+++ index.ts
@@ -1,22 +1,45 @@
-function formatString(input: string, toUpper?: boolean): string{
-    if(toUpper===false){
-        return input.toLowerCase();
-    }
-    else{
-        return input.toUpperCase();
-    }
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLowerCase();
+  } else {
+    return input.toUpperCase();
+  }
 }
 
-function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
-    return items.filter((item: { title: string; rating: number })=> item.rating >=4)
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter(
+    (item: { title: string; rating: number }) => item.rating >= 4
+  );
 }
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
-    const result: T[] = [];
-    for (const arr of arrays) {
-      for (const item of arr) {
-        result.push(item);
-      }
+  const result: T[] = [];
+  for (const arr of arrays) {
+    for (const item of arr) {
+      result.push(item);
     }
-    return result;
+  }
+  return result;
 }
+
+class Vehicle {
+  constructor(private make: string, private year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  getInfo() {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+class Car extends Vehicle {
+  constructor(make: string, year: number, private model: string) {
+    super(make, year);
+    this.model = model;
+  }
+  getModel() {
+    return `Model: ${this.model}`;
+  }
+}
+
