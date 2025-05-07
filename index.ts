31e8b6607278a9da01a2795da647eb6ab7e24203
Emon <emon.mhk69@gmail.com>
2025-05-07 21:36:08 +0600

    problem-3 done

--- index.ts
+++ index.ts
@@ -10,3 +10,13 @@ function formatString(input: string, toUpper?: boolean): string{
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
     return items.filter((item: { title: string; rating: number })=> item.rating >=4)
 }
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    const result: T[] = [];
+    for (const arr of arrays) {
+      for (const item of arr) {
+        result.push(item);
+      }
+    }
+    return result;
+}
